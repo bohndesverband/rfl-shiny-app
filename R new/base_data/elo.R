0b1f5731526fd025ee0f455e70077f97d8eb0cef
Jakob Eschler <mail@jakob-eschler.de>
2025-09-23 01:58:29 +0200

    fixed elo script

--- R new/base_data/elo.R
+++ R new/base_data/elo.R
@@ -5,7 +5,8 @@ rfl_matchups_history <- team_elo %>%
   mutate(
     elo_diff = franchise_elo_pregame - opponent_elo_pregame,
     upset = ifelse(elo_diff < 0 & score_diff > 0, 1, 0),
-    label = paste0(franchise_name, " (", franchise_elo_pregame, ") W über ", opponent_name, " (", opponent_elo_pregame, ") - WK", week, " ", season)
+    result = ifelse(score_diff > 0, " W über ", " L gegen "),
+    label = paste0(franchise_name, " (", franchise_elo_pregame, ")", result, opponent_name, " (", opponent_elo_pregame, ") - WK", week, " ", season)
   ) %>%
   dplyr::select(season, week, franchise_id, opponent_id, franchise_name, elo_shift, franchise_elo_pregame, franchise_score, elo_diff, opponent_score, opponent_elo_pregame, opponent_name, upset, label)
 
